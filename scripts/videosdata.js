721eb066b1b49d5e01edd145d8ba4dac8d854cb5
xaamus <93161575+xaamus@users.noreply.github.com>
2023-11-13 22:15:41 +0100

    Add files via upload

---
+++ scripts/videosdata.js
@@ -0,0 +1,194 @@
+let titleMarvel = [
+    'the guardians of the galaxy vol 3',
+    'secret invasion',
+    'ant man and the wasp quantumania',
+    'black panther wakanda forever',
+    'the guardians of the galaxy holiday special',
+    'werewolf by night',
+    'she hulk attorney at law',
+    'i am groot',
+    'thor love and thunder',
+    'ms marvel',
+    'doctor strange in the multiverse of madness',
+    'moon knight',
+    'spider man no way home',
+    'hawkeye',
+    'eternals',
+    'shang chi and the legend of the ten rings',
+    'what if',
+    'black widow',
+    'loki',
+    'the falcon and the winter soldier',
+    'wandavision',
+    'spider man far from home',
+    'avengers endgame',
+    'captain marvel',
+    'ant man and the wasp',
+    'avengers infinity war',
+    'black panther',
+    'thor ragnarok',
+    'spider man homecoming',
+    'the guardians of the galaxy vol 2',
+    'doctor strange',
+    'captain america civil war',
+    'ant man',
+    'avengers age of ultron',
+    'the guardians of the galaxy',
+    'captain america the winter soldier',
+    'thor the dark world',
+    'iron man 3',
+    'the avengers',
+    'captain america the first avenger',
+    'thor',
+    'iron man 2',
+    'the incredible hulk',
+    'iron man'
+];
+let yearMarvel = [
+    '23','23','23','22',
+    '22','22','22','22',
+    '22','22','22','22',
+    '21','21','21','21',
+    '21','21','21','21',
+    '21','19','19','19',
+    '18','18','18','17',
+    '17','17','16','16',
+    '15','15','14','14',
+    '13','13','12','11',
+    '11','10','08','08'
+];
+let titleStarWars = [
+    'obi wan kenobi',
+    'the book of boba fett',
+    'star wars visions',
+    'star wars the bad batch',
+    'star wars the rise of skywalker',
+    'the mandalorian',
+    'solo a star wars story',
+    'star wars the last jedi',
+    'rogue one a star wars story',
+    'star wars the force awakens',
+    'star wars rebels',
+    'star wars the clone wars',
+    'star wars revenge of the sith',
+    'star wars attack of the clones',
+    'star wars the phantom menace',
+    'star wars return of the jedi',
+    'star wars the empire strikes back',
+    'star wars a new hope'
+];
+let yearStarWars = [
+    '22','21','21','21',
+    '19','19','18','17',
+    '16','15','14','08',
+    '05','02','99','83',
+    '80','77'
+];
+let titlePixar = [
+    'lightyear',
+    'turning red',
+    'dug days',
+    'monsters at work',
+    'luca',
+    'soul',
+    'onward',
+    'toy story 4',
+    'the incredibles 2',
+    'coco',
+    'cars 3',
+    'finding dory',
+    'the good dinosaur',
+    'inside out',
+    'planes fire and rescue',
+    'planes',
+    'monsters university',
+    'brave',
+    'cars 2',
+    'toy story 3',
+    'up',
+    'wall-e',
+    'ratatouille',
+    'cars',
+    'the incredibles',
+    'finding nemo',
+    'monsters inc',
+    'toy story 2',
+    'a bugs life',
+    'toy story'
+];
+let yearPixar = [
+    '22','22','21','21',
+    '21','20','20','19',
+    '18','17','17','16',
+    '15','15','14','13',
+    '13','12','11','10',
+    '09','08','07','06',
+    '04','03','01','99',
+    '98','95'
+];
+let titleHarryPotter = [
+    'fantastic beasts the secrets of dumbledore',
+    'fantastic beasts the crimes of grindelwald',
+    'fantastic beasts and where to find them',
+    'harry potter and the deathly hallows part 2',
+    'harry potter and the deathly hallows part 1',
+    'harry potter and the half blood prince',
+    'harry potter and the order of the phoenix',
+    'harry potter and the goblet of fire',
+    'harry potter and the prisoner of azkaban',
+    'harry potter and the chamber of secrets',
+    'harry potter and the philosophers stone'
+];
+let yearHarryPotter = [
+    '22','18','16','11',
+    '10','09','07','05',
+    '04','03','02'
+];
+let titleTheLordOfTheRings = [
+    'the lord of the rings the fellowship of the ring',
+    'the lord of the rings the two towers',
+    'the lord of the rings the return of the king',
+    'the hobbit an unexpected journey',
+    'the hobbit the desolation of smaug',
+    'the hobbit the battle of the five armies'
+]
+let yearTheLordOfTheRings = [
+    '14','13','12','03',
+    '02','01'
+];
+let titleFastAndFurious = [
+    'f9',
+    'fast and furious presents hobbs and shaw',
+    'the fate of the furious',
+    'furious 7',
+    'fast and furious 6',
+    'fast five',
+    'fast and furious',
+    'the fast and the furious tokyo drift',
+    '2 fast 2 furious',
+    'the fast and the furious'
+]
+let yearFastAndFurious = [
+    '21','19','17','15',
+    '13','11','09','06',
+    '03','01'
+];
+let titleVoid = [
+    'lego ninjago',
+    'rick and morty',
+    'formula 1',
+    'null',
+    'null',
+    'null',
+    'null',
+    'null',
+    'null',
+    'null',
+    'null',
+    'null'
+];
+let yearVoid = [
+    '11','13','21','',
+    '','','','',
+    '','','',''
+];
